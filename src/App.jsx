02c2dbf743b994684ba54cce02121b010c54c300
Kyle Greenidge <kylegreen149@gmail.com>
2025-08-25 20:35:53 -0400

    Update search component to handle form data, use handleSubmit to make an API request to find location based on user's input

--- src/App.jsx
+++ src/App.jsx
@@ -5,31 +5,40 @@ import Search from './Search'
 
 function App() {
   const [clicked, setClicked] = useState(false)
-  const [data, setData] = useState([])
+  const [data, setData] = useState(null)
   const [location, setLocation] = useState("")
-  const getWeather = (location) => {
-    const apiKey = import.meta.env.VITE_WEATHER_API_KEY; // Using import.meta to support Vite
-
-    const baseURL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=${apiKey}`
-
-    axios
-      .get(baseURL)
-      .then(res => {
-        // console.log(res.data)
-        setData(res.data)
-      })
-
-  }
+  
 
   const handleChange = (e) => {
     console.log(e.target.value)
     setLocation(e.target.value)
   }
 
+  const handleSubmit = (e, location) => {
+    e.preventDefault()
+
+    setClicked(true)
+    const apiKey = import.meta.env.VITE_WEATHER_API_KEY; // Using import.meta to support Vite
+    const baseURL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=${apiKey}`
+
+    axios
+      .get(baseURL)
+      .then(res => {
+        console.log(res.data)
+        setData(res.data)
+      })
+      .catch(err => console.error("Weather fetch failed:", err))
+  }
   
   return (
     <div>
-      <Search location={location} handleChange={handleChange} />
+      <Search location={location} handleChange={handleChange} handleSubmit={(e) => handleSubmit(e, location)}/>
+      {data && (
+        <p>
+          {data.main ? `${data.name}: ${data.main.temp}°F, with ${data.weather[0].main} skies. The wind speed is ${data.wind.speed} mph` : "Loading..."}
+        </p>
+      )}
+
     </div>
   )
 }
--- src/Search.jsx
+++ src/Search.jsx
@@ -1,9 +1,10 @@
-const Search = ({location, handleChange}) => {
+const Search = ({location, handleChange, handleSubmit}) => {
     return (
-        <form className="search-form">
+        <form className="search-form" onSubmit={handleSubmit}>
             <input 
-                onChange={handleChange}
+                type="text"
                 value={location}
+                onChange={handleChange}
                 placeholder="Search for a city here..."
             />
             <button>Search</button>
